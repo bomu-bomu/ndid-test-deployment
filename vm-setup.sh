b0a0bf6595394af0094cf75098a6e1dbeabba81c
bomu.bomu <teerayut.h@outlook.co.th>
2018-07-12 13:38:23 +0000

    Add setup for azure-cli and terraform

--- vm-setup.sh
+++ vm-setup.sh
@@ -2,21 +2,30 @@ apt-get install -y\
     apt-transport-https \
     ca-certificates \
     curl \
-    software-properties-common
+    software-properties-common \
+    jq \
+    unzip
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-sudo add-apt-repository \
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 
+AZ_REPO=$(lsb_release -cs)
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
 apt-get update 
-apt-get install -y docker-ce
+apt-get install -y docker-ce azure-cli
 
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-
 chmod 755 /usr/local/bin/docker-compose
 
-cd /
-git clone https://github.com/bomu-bomu/ndid-test-deployment.git
+wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
+unzip terraform_0.11.7_linux_amd64.zip
+mv terraform /usr/local/bin
+rm terraform_0.11.7_linux_amd64.zip
+
