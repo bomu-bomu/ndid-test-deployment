0c43482f2c052415400f8128722906d10fbd54a4
bomu.bomu <teerayut.h@outlook.co.th>
2018-07-14 17:19:58 +0700

    Add machine manipulation scripts

---
+++ scripts/machine.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+command=$1
+AZ_RESOURCE_GROUP="AKSTesting"
+NODE_PREFIX="tm-node"
+NODE_COUNT=10
+
+start_vm() {
+  echo -n "Start VM: "
+  i=0
+  while [ $i -lt $NODE_COUNT ]
+  do
+    az vm start -g $AZ_RESOURCE_GROUP -n $NODE_PREFIX-$i --no-wait
+    i=`expr $i + 1`
+    echo -n "."
+  done
+  echo " OK"
+}
+
+stop_vm() {
+  echo -n "Stop VM: "
+  i=0
+  while [ $i -lt $NODE_COUNT ]
+  do
+    az vm deallocate -g $AZ_RESOURCE_GROUP -n $NODE_PREFIX-$i --no-wait
+    i=`expr $i + 1`
+    echo -n "."
+  done
+  echo " OK"
+}
+
+case $command in
+  start)
+    start_vm
+    ;;
+  stop)
+    stop_vm
+    ;;
+  update)
+    echo "Update vm nodes to latest"
+    ansible $NODE_PREFIX -m apt -a "update_cache=yes upgrade=yes" --become --fork=10
+  *)
+    echo "Usage: $0 <start|stop|update>"
+    ;;
+esac
